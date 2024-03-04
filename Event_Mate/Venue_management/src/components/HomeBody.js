fb25362b91fe83ede0c3492f66712b7fdb16cb61
ADITHYA CHERUKURI <92886634+adithya-6101@users.noreply.github.com>
2024-03-03 20:56:36 -0500

    Updated HomeBody.js

--- Event_Mate/Venue_management/src/components/HomeBody.js
+++ Event_Mate/Venue_management/src/components/HomeBody.js
@@ -1,11 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect  } from "react";
+import { Link,useNavigate } from "react-router-dom";
 import img from "../assets/HomeImage.jpg";
 import bgimg from "../assets/backgroundImage.jpg";
 import { Button } from "./Button";
 import './homeBody.css';
+import ReservationsCard from "./ReservationsCard";
 
 const HomeBody = () => {
+    const navigate = useNavigate();
+    const [search, setSearch] = useState('');
+    const [reservations, setReservations] = useState([]);
+    const userId = "dummyUserId123";
+    // const [profileData, setProfileData] = useState({
+    //     user_id: "dummyUserId123",
+    //     username: "dummyUsername",
+    //     email: "dummy@example.com",
+    //     role: "user"
+    // });
+  
 
     return (
         <div className="homeContainer">
@@ -19,6 +31,33 @@ const HomeBody = () => {
                         </Button>
                     </Link>
                 </div>
+
+                {userId != null && <div className="ChildLeft">
+                    <h3>Reservations:</h3>
+                    <div className='ReservationSearch'>
+                      <input className="searchInput" placeholder="Search Reservations" onChange={(e) => setSearch(e.target.value)}/>
+                    </div>
+                    {reservations.filter((item) => {
+                    return search.toLowerCase() === '' ? item 
+                        : 
+                        (item.username.toLowerCase().includes(search)
+                        ||
+                        item.start_datetime.toLowerCase().includes(search)
+                        ||
+                        item.end_datetime.toLowerCase().includes(search));
+                    })
+                    .map((reservation) => (
+                        <ReservationsCard
+                          key={reservation.venue_id}
+                          id={reservation.venue_id}
+                          vname={reservation.vname}
+                          start_datetime={reservation.start_datetime}
+                          end_datetime={reservation.end_datetime}
+                          value_paid={reservation.value_paid === 1 ? 'Paid' : 'Not Paid'}
+                        />
+                    ))}
+                  </div>}
+
             </div>
         </div>
     )
