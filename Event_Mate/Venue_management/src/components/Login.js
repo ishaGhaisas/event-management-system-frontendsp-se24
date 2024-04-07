78af0be902678f3876cad0e62b55d4b139c2103f
srisaisreya <156003439+srisaisreya@users.noreply.github.com>
2024-04-06 20:33:59 -0400

    login changes

--- Event_Mate/Venue_management/src/components/Login.js
+++ Event_Mate/Venue_management/src/components/Login.js
@@ -1,162 +1,112 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import logo from "../assets/logo.png"
-
-
+import logo from "../assets/logo.png";
 
 const Login = () => {
-    const [loginInfo, setLoginInfo] = useState(false);
-    const navigate = useNavigate();
     const [info, setInfo] = useState({});
+    const [errorMessage, setErrorMessage] = useState("");
+    const navigate = useNavigate();
     const API_URL = process.env.REACT_APP_API_URL;
 
     const handleChange = (event) => {
-        console.log("handleChange called");
-        event.preventDefault();
         const name = event.target.name;
         const value = event.target.value;
-        
-        setInfo(values => ({...values, [name]: value}))
-        console.log(value);
-    }
+        setInfo((values) => ({ ...values, [name]: value }));
+    };
 
-    const fetchLogin = async(e) => {
-    
+    const fetchLogin = async (e) => {
+        e.preventDefault();
+
         try {
             const response = await fetch(`${API_URL}/auth/login`, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(info),
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(info),
             });
 
             if (response.ok) {
                 const data = await response.json();
-                info.email = data.user.email;
-                window.localStorage.setItem("token", "Bearer " + data.authorization);
                 console.log("Login successful:", data);
+                // Redirect to homepage with username as query parameter
+                navigate(`/?username=${info.email}`);
             } else {
-              const errorData = await response.json();
-              console.error("Login failed:", errorData);
+                const errorData = await response.text();
+                console.error("Login failed:", errorData);
+                setErrorMessage(errorData || "Login failed. Please try again.");
             }
-         } catch (error) {
+        } catch (error) {
             console.error("Login error:", error);
+            setErrorMessage("Login failed. Please try again."); // Display a generic error message
         }
-
-        setLoginInfo(true);
-    }
-
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-
-        try {
-            const response = await fetch(`${API_URL}/auth/2fa/verify`, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-                "Authorization": window.localStorage.getItem("token"),
-              },
-              body: JSON.stringify(info),
-            });
-      
-            if (response.ok) {
-              const data = await response.json();
-              console.log("2fa verify successful:", data);
-              window.localStorage.setItem("userId", data.user.user_id);
-              window.localStorage.setItem("role", data.user.role);
-              window.localStorage.setItem("userEmail", data.user.email);
-              window.localStorage.setItem("username", data.user.username);
-              window.localStorage.setItem("token", "Bearer " + data.authorization);
-              navigate("/");
-              window.location.reload();
-            } else {
-              const errorData = await response.json();
-              console.error("2fa verify failed:", errorData);
-            }
-          } catch (error) {
-            console.error("2fa verify error:", error);
-          }
     };
-    
 
-    const gotoSignUpPage = () => navigate("/register"); 
-    const gotoForgetPage = () => navigate("/forget")
+    const gotoSignUpPage = () => navigate("/register");
+    const gotoForgetPage = () => navigate("/forget");
 
     return (
-        
         <div className="loginBody">
-        <div className='loginContainer'>
-            <div className="logo">
-                <img src={logo }width={250} height={85} alt='Logo'></img>
+            <div className="loginContainer">
+                <div className="logo">
+                    <img src={logo} width={250} height={85} alt="Logo" />
+                </div>
+                <h2>Login</h2>
+
+                <form className="loginForm" data-testid="loginForm">
+                    <div className="inputBox">
+                        <i className="bx bxs-envelope" />
+                        <input
+                            placeholder="Email"
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={info.email}
+                            required
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="inputBox">
+                        <i className="bx bxs-lock-alt"></i>
+                        <input
+                            placeholder="Password"
+                            type="password"
+                            name="password"
+                            id="password"
+                            minLength={8}
+                            required
+                            value={info.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <button className="loginBtn" onClick={fetchLogin}>
+                        Login
+                    </button>
+
+                    {errorMessage && (
+                        <p className="error" style={{ color: "red" }}>
+                            <span className="error-text">{errorMessage}</span>
+                        </p>
+                    )}
+
+                    <div className="forgot">
+                        Forgot password?{" "}
+                        <Link to="/forgot-password" className="forgotPasswordLink" onClick={gotoForgetPage}>
+                            Click here
+                        </Link>
+                    </div>
+
+                    <p>
+                        Don't have an account?{" "}
+                        <span className="link" data-testid="toSignin" onClick={gotoSignUpPage}>
+                            Sign up
+                        </span>
+                    </p>
+                </form>
             </div>
-            <h2>Login </h2>
-
-            <form className='loginForm' data-testid='loginForm' onSubmit={handleSubmit}>
-                <div className="inputBox">
-                    <i className='bx bxs-envelope'/>
-                    <input
-                        placeholder="Username"
-                        type='text'
-                        id='username'
-                        name='username'
-                        value={info.username}
-                        required
-                        onChange={handleChange}
-                    />
-
-                </div>
-                <div className="inputBox">
-                    <i className="bx bxs-lock-alt"></i>
-                    <input
-                        placeholder="Password"
-                        type='password'
-                        name='password'
-                        id='password'
-                        minLength={8}
-                        required
-                        value={info.password}
-                        onChange={handleChange}
-                    />
-                </div>
-                
-                <button className="loginBtn" onClick={fetchLogin}> Login </button>
-
-                { loginInfo &&
-                    <>
-                        <div className="inputBox">
-                            <i className="bx bxs-lock-alt"></i>
-                            <input
-                                placeholder="Enter Emailed Code"
-                                type='text'
-                                name='Otp'
-                                id='Otp'
-                                minLength={2}
-                                required
-                                value={info.Otp}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <button className="loginBtn" onClick={handleSubmit}> Submit </button>
-                    </>
-                }
-
-                <div className="forgot">
-                    Forgot password?{" "}
-                    <Link to="/forgot-password" className="forgotPasswordLink" onClick={gotoForgetPage}>
-                        Click here
-                    </Link>
-                </div>
-
-                <p>
-                    Don't have an account?{" "}
-                    <span className='link' data-testid="toSignin" onClick={gotoSignUpPage}>
-                        Sign up
-                    </span>
-                </p>
-            </form>
-        </div>
         </div>
     );
 };
+
 export default Login;
