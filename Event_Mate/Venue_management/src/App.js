caf06895d3113f39322d1ebef6a053462be7ac47
ADITHYA CHERUKURI <92886634+adithya-6101@users.noreply.github.com>
2024-04-21 19:45:21 -0400

    Update App.js

--- Event_Mate/Venue_management/src/App.js
+++ Event_Mate/Venue_management/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
 import HomeBody from "./components/HomeBody.js";
+import InviteFriend from "./components/inviteFriend.js";
+
 //import "./index.css";
 import NavBar from "./components/navBar";
 import Home from './components/Home.js'
@@ -16,6 +18,12 @@ import VenueDetails from "./components/venueDetails.js";
 import VenuesTable from "./components/ViewPage.js";
 import ReviewBooking from "./components/reviewBooking.js";
 import Paymethod from "./components/Payment.js";
+import FriendInfo from "./components/friend_info.js";
+import UserPage from "./components/userPage.js";
+import EditProfileDetails from "./components/editProfileDetails.js";
+import OwnerView from "./components/OwnerView.js";
+import AddVenueForm from "./components/AddVenueForm.js";
+
 
 
 
@@ -38,6 +46,11 @@ function App() {
           <Route path='/review-booking?' element={<ReviewBooking />}/>
           <Route path='/payment' element={<Paymethod />}/>
           <Route path='/venue/venue-details?' element={<VenueDetails />}/>
+         <Route path='/edit-profile-details' element={<EditProfileDetails />} />
+          <Route path='/invite-friend' element={<InviteFriend />} />
+          <Route path='/friend_info' element={<FriendInfo/>} />
+          <Route path='/OwnerView' element={<OwnerView/>} />
+          <Route path='/AddVenueForm' element={<AddVenueForm/>}/>
       </Routes>
     </BrowserRouter>
 
