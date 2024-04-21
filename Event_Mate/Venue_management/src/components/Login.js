bd0f59a8cbfd5e104f4392335ea9c6556c16a409
ADITHYA CHERUKURI <92886634+adithya-6101@users.noreply.github.com>
2024-04-21 19:07:42 -0400

    Update Login.js

--- Event_Mate/Venue_management/src/components/Login.js
+++ Event_Mate/Venue_management/src/components/Login.js
@@ -28,12 +28,20 @@ const Login = () => {
                 },
                 body: JSON.stringify(info),
             });
+
+             window.localStorage.setItem('username', info.email);
+            
             if (response.status === 200) {
                 const data = await response.json();
                 console.log("Login successful:", data);
             
                 if (data.status === "pending") {
                     console.log(info)
+
+                    
+                    window.localStorage.setItem("user_ID", data.data.userId);
+                    window.localStorage.setItem('email', data.data.email);
+                    
                     window.localStorage.setItem('username', info.email);
                     window.localStorage.setItem('userId', data.data.userId);
                     
